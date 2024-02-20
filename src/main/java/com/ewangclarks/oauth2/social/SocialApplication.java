99e376df1007c1a1bd9535983b94d13238248c6e
ewangclarkson <ewangclarks@gmail.com>
2024-02-20 20:19:30 +0100

    abstracted the env properties

--- src/main/java/com/ewangclarks/oauth2/social/SocialApplication.java
+++ src/main/java/com/ewangclarks/oauth2/social/SocialApplication.java
@@ -2,8 +2,10 @@ package com.ewangclarks.oauth2.social;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:${user.dir}/.env")
 public class SocialApplication {
 
 	public static void main(String[] args) {
